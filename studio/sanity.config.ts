07f373f531d7cad26cf4961e0271777a1e380675
andperks <andperks6@gmail.com>
2023-09-02 12:01:22 -0600

    studio config

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -2,16 +2,17 @@ import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {schemaTypes} from './schemas'
+import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash'
 
 export const projectId = process.env.SANITY_STUDIO_PROJECT_ID!
 export const dataset = process.env.SANITY_STUDIO_DATASET!
 
 export default defineConfig({
-  name: 'project-name',
-  title: 'Project Name',
+  name: 'otter-eats',
+  title: 'Otter Eats',
   projectId,
   dataset,
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(), unsplashImageAsset()],
   schema: {
     types: schemaTypes,
   },
