4a17cee7103476a53f5241b94318cb6a91b51889
andperks <andperks6@gmail.com>
2023-09-24 21:06:52 -0600

    card component

--- app/src/lib/Card.svelte
+++ app/src/lib/Card.svelte
@@ -9,29 +9,39 @@
 	$: route = isRecipe(content) ? 'recipes' : 'techniques';
 </script>
 
-<div class="card">
-	{#if content.mainImage}
-		<img
-			class="card__cover"
-			src={urlFor(content.mainImage).width(500).height(300).url()}
-			alt="Cover image for {content.title}"
-		/>
-	{:else}
-		<div class="card__cover--none" />
-	{/if}
-
-	<div class="card__container">
-		<h3 class="card__title">
-			<a class="card__link" href={`/${route}/${content.slug.current}`}>
-				{content.title}
-			</a>
-		</h3>
-		{#if isRecipe(content)}
-		<p class="card__excerpt">{content.excerpt}</p>
-
+<a class="flex" href={`/${route}/${content.slug.current}`}>
+	<div class="cardLink card-compact sm:card flex flex-col bg-base-200 border-b-2 border-primary">
+		{#if content.mainImage}
+			<div class=" max-w-full">
+				<img
+					src={urlFor(content.mainImage).width(300).height(300).url()}
+					alt="Cover image for {content.title}"
+				/>
+			</div>
 		{/if}
-		<p class="card__date">
-			{formatDate(content._createdAt)}
-		</p>
+		<div class="card-body h-auto justify-between">
+			<div class="flex items-center justify-between" style="flex-wrap: wrap;">
+				<h2 class="card-title flex-1">
+					{content.title}
+				</h2>
+				{#if isRecipe(content)}
+					{#each content.categories as cat}
+						<div class="badge badge-secondary justify-end">{cat}</div>
+					{/each}
+				{/if}
+			</div>
+			{#if isRecipe(content)}
+				<div class="grow flex flex-col">
+					<div class="max-h-16 whitespace-normal truncate">{content.excerpt ?? ' '}</div>
+				</div>
+				{#if content.tags}
+					<div class="card-actions">
+						{#each content.tags as cat}
+							<div class="badge badge-outline">{cat}</div>
+						{/each}
+					</div>
+				{/if}
+			{/if}
+		</div>
 	</div>
-</div>
+</a>
